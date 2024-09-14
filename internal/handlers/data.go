8bc7378f4e39bc9dea560e639ef71dc1a26acac2
Elielson André <elielsonandre123@gmail.com>
2024-09-14 17:03:56 -0300

    refact: main instance

---
+++ internal/handlers/data.go
@@ -0,0 +1,38 @@
+package handlers
+
+import (
+	"net/http"
+
+	"https://github.com/elielson-and/golang-project-management/internal/db"
+
+	"github.com/labstack/echo/v4"
+)
+
+type CreateDataRequest struct {
+	Content string `json:"content"`
+}
+
+func CreateData(c echo.Context) error {
+	req := new(CreateDataRequest)
+	if err := c.Bind(req); err != nil {
+		return c.JSON(http.StatusBadRequest, "Invalid request")
+	}
+
+	data, err := db.CreateExampleData(c.Request().Context(), req.Content)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, "Failed to create data")
+	}
+
+	return c.JSON(http.StatusOK, data)
+}
+
+func GetData(c echo.Context) error {
+	id := c.Param("id")
+
+	data, err := db.GetExampleData(c.Request().Context(), id)
+	if err != nil {
+		return c.JSON(http.StatusNotFound, "Data not found")
+	}
+
+	return c.JSON(http.StatusOK, data)
+}
