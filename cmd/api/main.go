e58cbb73f7beafc17ac9922dc09fb01c1394d57d
Elielson André <elielsonandre123@gmail.com>
2024-09-14 17:19:59 -0300

    refac

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,16 +3,36 @@ package main
 import (
 	"net/http"
 
+	_ "github.com/elielson-and/golang-project-management/docs" // Import necessário para o Swag
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	echoSwagger "github.com/swaggo/echo-swagger" // Importa o Echo Swagger
 )
 
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample server for Swagger.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8080
+// @BasePath /
 func main() {
 	e := echo.New()
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
+	// Adiciona o endpoint Swagger
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
+
+	// Endpoint de teste
 	e.GET("/ping", func(c echo.Context) error {
 		return c.String(http.StatusOK, "pong")
 	})
